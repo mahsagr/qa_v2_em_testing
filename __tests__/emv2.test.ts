a2932125ac12641bc7b390f0e5f1e24f64cb5b6a
Mahsa Ghahri <mahsagh@Mahsas-Air.attlocal.net>
2021-05-05 23:17:01 -0500

    revert complete assignement fot emv2

--- __tests__/emv2.test.ts
+++ __tests__/emv2.test.ts
@@ -1,6 +1,31 @@
 import { EmployeeManager, Employee } from "./pageObjects/EmployeeManager";
-import * as employees from "./employee.json";
 
+const employees: Array<Employee> = [
+  {
+    name: "Han Solo",
+    phone: 1111111111,
+    email: "millenium@falcon.rep",
+    title: "Smuggler",
+  },
+  {
+    name: "Luke Skywalker",
+    phone: 2222222222,
+    email: "red5@rogue.rep",
+    title: "Jedi",
+  },
+  {
+    name: "Thrawn",
+    phone: 3333333333,
+    email: "gathrawn@admiralty.emp",
+    title: "Best Villain",
+  },
+  {
+    name: "R2-D2",
+    phone: 4444444444,
+    email: "pottymouth@astromech.rep",
+    title: "Crotchety Old Droid",
+  },
+];
 
 describe("employee manager v2", () => {
   const page = new EmployeeManager({ browser: "chrome" });
@@ -16,47 +41,21 @@ describe("employee manager v2", () => {
     let resultList = await page.getEmployeeList();
     expect(originalList.length).toBeGreaterThanOrEqual(resultList.length);
   });
-
-  employees.forEach((item)=>{  
-    test("Can add and delete an employee", async () => {
-    await page.addEmployee(item);
-    let employee = await page.getCurrentEmployee();
-    expect(employee.name).toEqual(item.name);
-    expect(employee.phone).toEqual(item.phone);
-    expect(employee.email).toEqual(item.email);
-    expect(employee.title).toEqual(item.title);
-    await page.deleteEmployee(item.name);
-    let employeeList = await page.getEmployeeList();
-    expect(employeeList).not.toContain(item.name);
-  });
-
-  })
-  
-  test("Can add,search and take screenshot of an employee", async () => {
+  test("Can add and delete an employee", async () => {
     let newEmployee = {
-      name: "Mahsa",
-      phone: 5555555555,
-      email: "mahsa@email.com",
-      title: "Screenshot",
+      name: "Test Employee",
+      phone: 1234567890,
+      email: "test@email.com",
+      title: "test person",
     };
-    
-    // Add a new employee
     await page.addEmployee(newEmployee);
     let employee = await page.getCurrentEmployee();
     expect(employee.name).toEqual(newEmployee.name);
     expect(employee.phone).toEqual(newEmployee.phone);
     expect(employee.email).toEqual(newEmployee.email);
     expect(employee.title).toEqual(newEmployee.title);
+    await page.deleteEmployee("Test Employee");
     let employeeList = await page.getEmployeeList();
-    // search for employee:
-    await page.searchFor("Screenshot");
-    let resultList = await page.getEmployeeList();
-    expect(employeeList.length).toBeGreaterThanOrEqual(resultList.length);
-    //  take screenshot of employee
-    await page.takeScreenshot("screenshot/Mahsapic");
+    expect(employeeList).not.toContain("Test Employee");
   });
-
- 
-      
-
 });
--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["__tests__/pageObjects/"],
-  setupFilesAfterEnv: ["./jest.setup.js"],        
-  testTimeout: 20000,
-
+  setupFilesAfterEnv: ["./jest.setup.js"],
 };
